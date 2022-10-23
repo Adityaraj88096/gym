39c5a6ab618a0befb0c7e66b50e42fb2c450ee65
Adityaraj88096 <Adityaraj88096@gmail.com>
2022-10-23 12:09:31 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+Welcome to this workout project.
+
+# start with installing the node modules
+    `npm install` or `yarn install`
+
+# Create a .env file and add a variables in it.
+    REACT_APP_DB_LINK(it is important to add REACT_APP as prefix or it won't work)
+
+# For dev mode :
+    Run `npm start` in your terminal.
+
+# For Prod mode: 
+    Run `npm run build`
+   You will get an optimized version and it's already being served on your local system or cloud.
---
+++ src/components/SearchExercises.js
@@ -0,0 +1,94 @@
+import React, { useState, useEffect } from 'react'
+import { Box, Button, Stack, TextField, Typography } from '@mui/material';
+import { fetchData } from '../utils/fetchData';
+import HorizontalScrollbar from './HorizontalScrollbar';
+const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
+  const [search, setSearch] = useState();
+  const [bodyParts, setBodyParts] = useState([]);
+  useEffect(() => {
+    const fetchExerciseData = async () => {
+      const {bodyparts} = await fetchData(`${process.env.REACT_APP_DB_LINK}/bodyparts`);
+      console.log(bodyparts);
+      setBodyParts(['all', ...bodyparts]);
+    }
+    fetchExerciseData();
+  }, [])
+
+  const handleSearch = async () => {
+    try {
+      if (search) {
+        const exerciseData = await fetchData(`${process.env.REACT_APP_DB_LINK}/exercises`);
+        const searchedExercises = exerciseData.filter(
+          (exercise) => {
+            return exercise.name.toLowerCase().includes(search)
+              || exercise.target.toLowerCase().includes(search)
+              || exercise.equipment.toLowerCase().includes(search)
+              || exercise.bodyPart.toLowerCase().includes(search)
+          }
+        );
+        setSearch('');
+        setExercises(searchedExercises);
+        window.scrollTo({ top: 1800, left: 100, behavior: 'smooth' })
+      }
+    }
+    catch (error) {
+      console.log(error)
+    }
+  }
+  return (
+    <>
+      <Stack alignItems="center" sx={{ mt: { lg: "2.4rem", xs: "4rem" } }}
+        justifyContent="center" p="1.25rem">
+        <Typography fontWeight={700}
+          sx={{ fontSize: { lg: '3.75rem', xs: '1.875rem' } }}
+          mb="3.125rem" textAlign="center"
+        >
+          Awesome Exercises You <br />
+          Should Know
+        </Typography>
+        <Box>
+          <TextField
+            sx={{
+              input: {
+                fontWeight: '700',
+                border: 'none',
+                borderRadius: '0.25rem',
+              },
+              width: { lg: '50rem', xs: '21.875rem' },
+              backgroundColor: '#fff',
+            }}
+            height="7.75rem"
+            onChange={(e) => {
+              const value = e.target.value.toLowerCase();
+              setSearch(value)
+            }}
+            placeholder="Search Exercises"
+            type="text"
+          />
+          <Button className="search-btn" sx={{
+            bgcolor: '#FF2625',
+            color: '#fff',
+            textTransform: 'none',
+            width: { lg: '11rem', xs: '5rem' },
+            fontSize: { lg: '1.25rem', xs: '0.875rem' },
+            height: '3.5rem',
+            position: 'absolute',
+          }}
+            onClick={handleSearch}
+          >
+            Search
+          </Button>
+        </Box>
+        <Box sx={{
+          position: 'relative',
+          width: '100vw',
+          p: '10%'
+        }}>
+          <HorizontalScrollbar data={bodyParts} bodyParts bodyPart={bodyPart} setBodyPart={setBodyPart} isBodyParts />
+        </Box>
+      </Stack>
+    </>
+  )
+};
+
+export default SearchExercises
